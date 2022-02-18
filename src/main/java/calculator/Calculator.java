29cb8c61ebd032b52c85fd42fd9a1ee677de4f30
csekok <csekok@gamma.ttk.pte.hu>
2022-02-18 08:22:09 +0100

    2. class

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,33 @@
 package calculator;
 
 public class Calculator {
+
+    public static String add(int a, int b) {
+        int result = a + b;
+        return "" + a + " + " + b + " = " + result;
+    }
+
+    public static String subtract(int a, int b) {
+        int result = a - b;
+        return "" + a + " - " + b + " = " + result;
+    }
+
+    public static String multiply(int a, int b) {
+        int result = a * b;
+        return "" + a + " * " + b + " = " + result;
+    }
+
+    public static String divide(int a, int b) {
+        if (b == 0) {
+            return "Cannot divide with 0.";
+        } else {
+            if (a % b == 0) {
+                int result = a / b;
+                return "" + a + " / " + b + " = " + result;
+            } else {
+                double result = (double) a / b;
+                return "" + a + " / " + b + " = " + result;
+            }
+        }
+    }
 }
