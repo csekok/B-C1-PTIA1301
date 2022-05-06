a0aacfb81dab7d9d40f1e9ae386a53845d3dcacc
csekok <csekok@gamma.ttk.pte.hu>
2022-05-06 07:58:52 +0200

    advance

---
+++ src/main/java/register/Advance.java
@@ -0,0 +1,96 @@
+package register;
+
+public class Advance {
+    /**
+     * Task 0: Update the project:
+     *          - From menu: Git / "Update Project...", OR
+     *          - In right upper corner: blue arrow, OR
+     *          - Keyboard shortcut: Ctrl+T
+     * In "Update the project" popup click on Ok button.
+     */
+
+    /**
+     * Task :
+     * Create lists of integers and strings in main method.
+     * Add values to lists.
+     * Create a printAllElements(list) method to print all elements
+     * of a list in separated rows.
+     * Hint: Use generics: ArrayList<T>
+     */
+
+    /**
+     * Task :
+     * In main method implement algorithm to separate users from a list,
+     * who is born before a given year.
+     * Hint: Use stream and lambda expression to filter users.
+     */
+
+    /**
+     * Task :
+     * Create a Greeting interface with greet and
+     * greetSomeone(someone) methods to say hello.
+     * Hint: To create a new interface you should use
+     *       "public interface <name of interface>" structure.
+     */
+
+    /**
+     * Task :
+     * Create sayHello(name) method, where:
+     *      - implement EnglishGreeting from Greeting
+     *      - implement both methods of EnglishGreeting with "Hello"
+     *      - create a Greeting variable with an EnglishGreeting object
+     *      - create a Greeting variable with a new Greeting object,
+     *        with Hungarian greeting "Szia"
+     *      - call both method of these local variables
+     * Call the sayHello method in main.
+     * Hint: To implement an interface use implements keyword and
+     *       the name of the interface.
+     */
+
+    /**
+     * Task :
+     * Create a new annotation named ClassHistory.
+     * Define 3 elements in it:
+     *       - author
+     *       - createdOn
+     *       - lastModifiedOn
+     * Hint: To create a new annotation you should use @interface
+     *       keyword.
+     * Hint: You can define default values with default keyword;
+     */
+
+    /**
+     * Task :
+     * Add ClassHistory annotation to our previously created classes.
+     * Hint: To add a new annotation to your class, you should insert
+     *       an @ character and the name of your annotation before
+     *       your class header row.
+     * Hint: If there are elements in an annotation you can enter
+     *       values to them in parenthesis, e.g.:
+     *       @ClassHistory(author = "Anonymous")
+     */
+
+    /**
+     * Task :
+     * In main method get and print the values of the elements of
+     * your annotation.
+     * Hint: To get annotations of a class, methods, etc. you should
+     *       use reflection: <classname>.class
+     */
+
+    /**
+     * Task :
+     * Create a MyCalculator interface with two method signature:
+     *       - void add(int x, int y)
+     *       - void sub(int x, int y)
+     */
+
+    /**
+     * Task :
+     * Implement the MyCalculator in MyCalculatorImp class.
+     * Create a calcMe(x, y) method, where:
+     *       - create a new MyCalculatorImp object and store
+     *         in a MyCalculator variable
+     *       - call its add and sub method with x and y
+     */
+}
